5c084d8853cd806b9737e977fe1899ed17716de7
Arman-2107046 <armanr.rafi@gmail.com>
2025-11-15 14:14:12 +0600

    Api route updated

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OneWayFlightController;
+
+
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// use Illuminate\Support\Facades\Route;
+
+// Customer auth
+Route::post('/customer/register', [AuthController::class, 'register']);
+Route::post('/customer/login',    [AuthController::class, 'login']);
+
+// Protected routes (need customer token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/customer/me', [AuthController::class, 'me']);
+
+    Route::post('/flight/one-way', [OneWayFlightController::class, 'store']);
+});
